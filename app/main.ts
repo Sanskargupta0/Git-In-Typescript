9239594a5fc90f1acb8054f8f765691efdb681f8
Sanskar gupta <Sanskar362002@gmail.com>
2025-07-25 10:33:08 +0530

    Write a tree object

--- app/main.ts
+++ app/main.ts
@@ -1,72 +1,28 @@
-import * as fs from 'node:fs';
-import zlib from "node:zlib";
-// import stream from "node:stream";
-// import crypto from "node:crypto";
-import process from "node:process";
-import path from "path";
+import * as fs from 'fs';
+import { read } from 'node:fs';
+import * as crypto from "crypto"
+import path from 'node:path';
+import { blob } from 'node:stream/consumers';
+import zlib from 'node:zlib';
 
-const NULL_BYTE = "\0";
-
-const Commands = {
-    Init: "init",
-    CatFile: "cat-file",
-    HashObject: "hash-object"
-    LsTrees: "ls-trees",
-} as const;
-
-const GIT = {
-    types: {
-        blob: "blob",
-    }
-} as const;
-
-type HashObjectArgs = [typeof Commands.HashObject, "-w", string];
-type CatFileArgs = [typeof Commands.CatFile, string, string];
-type LsTreesArgs = [typeof Commands.LsTrees, string];
 
 const args = process.argv.slice(2);
 const command = args[0];
 
-const exit = (error: Error | undefined = undefined) => {
-    if (!error) {
-        process.exit(0);
-    } else {
-        process.stderr.write(error.message);
-        process.exit(1);
-    }
-}
 
-const print = (message: string) => process.stdout.write(message);
+//commands
 
-const findNearbyGitDir = (initialPath: string) => {
-    if (!fs.existsSync(initialPath)) exit(new Error(`Path ${initialPath} does not exist`));
-    let gitDir = "";
 
-    fs.readdirSync(initialPath).forEach((file) => {
-        if (file === ".git") {
-            gitDir = path.resolve(initialPath, file);
-        }
-    });
-
-    if (gitDir) return gitDir;
-
-    const parentDir = path.resolve(initialPath, "..");
-    if (parentDir === "/") exit(new Error(`No .git directory found in ${initialPath}`));
-
-    return findNearbyGitDir(parentDir);
-}
-
-const handleError = <T>(fn: () => T) => {
-    try {
-        return fn();
-    } catch (error) {
-        exit(error as Error);
-        return undefined as T;
-    }
+enum Commands {
+    Init = "init",
+    Cat = "cat-file",
+    Hash = "hash-object",
+    Tree = "ls-tree",
+    Write = "write-tree",
 }
 
 switch (command) {
-    case Commands.Init: {
+    case Commands.Init:
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         console.error("Logs from your program will appear here!");
 
@@ -77,69 +33,192 @@ switch (command) {
         fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
         console.log("Initialized git directory");
         break;
-    }
-    case Commands.CatFile: {
-        const dotGitPath = findNearbyGitDir(process.cwd());
 
-        const commandArgs = args as CatFileArgs;
-        const objectHash = commandArgs[2];
-        const objectFilepath = `${dotGitPath}/objects/${objectHash.slice(0, 2)}/${objectHash.slice(2)}`;
+    case Commands.Cat:
+         handleCatFileCommand();
+         break;
 
-        const blob = await Bun.file(objectFilepath).arrayBuffer();
-        const decompressedBuffer = handleError(() => zlib.unzipSync(blob));
-        const nullByteIndex = decompressedBuffer.indexOf(NULL_BYTE);
-        const blobContent = decompressedBuffer.subarray(nullByteIndex + 1).toString();
+    case Commands.Hash:
+        handleHashCommand();
+        break; 
 
-        print(blobContent);
+    case Commands.Tree:
+        handleTreeInspectCommand();
         break;
-    }
-    case Commands.HashObject: {
-        const dotGitPath = findNearbyGitDir(process.cwd());
 
-        const commandArgs = args as HashObjectArgs;
-        const filepath = path.resolve(commandArgs[2]);
-
-        if (!await Bun.file(filepath).exists()) {
-            exit(new Error(`File ${filepath} does not exist`));
-        }
-
-        const fileContent = await Bun.file(filepath).arrayBuffer();
-        const header = await new Blob([`${GIT.types.blob} ${fileContent.byteLength}${NULL_BYTE}`]).arrayBuffer();
-        const resultingObjectBuffer = await new Blob([header, fileContent]).arrayBuffer();
-
-        const hash = new Bun.CryptoHasher("sha1").update(resultingObjectBuffer).digest("hex");
-
-        const objectHash = `${hash.slice(0, 2)}/${hash.slice(2)}`;
-        const objectFilepath = `${dotGitPath}/objects/${objectHash}`;
-
-        fs.mkdirSync(path.dirname(objectFilepath), { recursive: true });
-
-        const compressed = zlib.deflateSync(resultingObjectBuffer);
-
-        const bytesWritten = await Bun.write(objectFilepath, new Uint8Array(compressed));
-        
-        if (bytesWritten !== compressed.byteLength) {
-            exit(new Error(`Failed to write object file ${objectFilepath}. Expected ${compressed.byteLength} bytes, wrote ${bytesWritten} bytes.`));
-        }
-
-        print(hash);
+    case Commands.Write:
+        handleWriteTreeCommand();
         break;
-    }
-    case Commands.LsTrees: {
-        const dotGitPath = findNearbyGitDir(process.cwd());
-
-        const commandArgs = args as LsTreesArgs;
-        const treeHash = commandArgs[2];
-        const treeFilepath = `${dotGitPath}/objects/${treeHash.slice(0, 2)}/${treeHash.slice(2)}`;
-
-        const tree = await Bun.file(treeFilepath).arrayBuffer();
-        const decompressedBuffer = handleError(() => zlib.unzipSync(tree));
-        const nullByteIndex = decompressedBuffer.indexOf(NULL_BYTE);
-        const treeContent = decompressedBuffer.subarray(nullByteIndex + 1).toString();
-
-        print(treeContent);
-        break;
-    }
     default:
         throw new Error(`Unknown command ${command}`);
+}
+
+function handleCatFileCommand(){
+    const folder=args[2].substring(0,2);
+    const file=args[2].substring(2);
+
+    const completePath= path.join(process.cwd(), ".git", "objects", folder, file);
+
+    const blob=fs.readFileSync(completePath);
+
+    const decompressedBuffer=zlib.unzipSync(blob);
+
+    const nullByteIndex=decompressedBuffer.indexOf(0);
+    const blobContent=decompressedBuffer.subarray(nullByteIndex+1).toString();
+
+    process.stdout.write(blobContent);
+}
+
+function generateHash(bufferVal: Buffer, type:string, write:boolean){
+     const header = `${type} ${bufferVal.length}\0`;
+     const blobStore= Buffer.concat([Buffer.from(header), bufferVal]);
+     const hash = crypto.createHash('sha1').update(blobStore).digest('hex');
+
+     if(write===true){
+        const folder=hash.slice(0,2);
+        const file=hash.slice(2);
+
+        const folderPath=path.join(process.cwd(), ".git", "objects", folder);
+        const filePath=path.join(folderPath, file);
+
+        fs.mkdirSync(folderPath, {recursive:true});
+        fs.writeFileSync(filePath, zlib.deflateSync(blobStore));
+     }
+     return hash ;
+}
+
+function handleHashCommand(){
+    const filePath= args[2];
+    const flag=args[1];
+
+    if(!filePath){
+        throw new Error(`Error: No file specified.`);
+    }
+
+    if(! fs.existsSync(filePath)){
+        throw new Error(
+            "could not open file path"
+        );
+    }
+
+    const readingContents=fs.readFileSync(filePath);
+
+    const fileLength=readingContents.length;
+
+    const blobHeader=`blob ${fileLength}\0` ;
+
+    const blob= Buffer.concat([Buffer.from(blobHeader), readingContents]);
+
+    const hash= crypto.createHash("sha1").update(blob).digest('hex');
+
+    if(flag==="-w" ){
+        const folder=hash.slice(0,2);
+        const file=hash.slice(2);
+
+        const completeFolderPath= path.join(".git", "objects", folder);
+
+        if(!fs.existsSync(completeFolderPath)){
+            fs.mkdirSync(completeFolderPath, {recursive:true});
+        }
+
+        const compressedData=zlib.deflateSync(blob);
+        fs.writeFileSync(path.join(completeFolderPath, file), compressedData);
+         
+    }
+    process.stdout.write(hash);
+
+}
+
+function handleTreeInspectCommand(){
+    const flag=args[1];
+    const commitSHA=args[2];
+
+    if (!commitSHA) {
+    throw new Error("Commit SHA is missing. Usage: ls-tree <commitSHA>");
+    }
+    const folder=commitSHA.slice(0,2);
+    const file=commitSHA.slice(2);
+    const folderPath=path.join(process.cwd(),".git","objects",folder);
+    const filePath=path.join(folderPath,file);
+
+    if(!fs.existsSync(folderPath))throw new Error(`Not a valid object name ${folderPath}`);
+    if(!fs.existsSync(filePath))throw new Error(`Not a valid object name ${filePath}`);
+
+    const fileContents=fs.readFileSync(filePath);
+    // console.log(fileContents);
+
+    const decompressed=zlib.inflateSync(fileContents);
+    const output=decompressed.toString("utf8");
+
+    const nullByteIdx=output.indexOf("\0");
+    if(nullByteIdx===-1)throw new Error("Incorrect tree format");
+    const content=output.slice(nullByteIdx+1);
+
+    let result= "";
+    let cursor= 0;
+
+    while(cursor < content.length){
+        const spaceIndex=content.indexOf(" ", cursor);
+        if(spaceIndex === -1)break;
+
+        const nullIndex=content.indexOf("\0",spaceIndex);
+        if(nullIndex === -1)break;
+
+        const fileName=content.slice(spaceIndex+1,nullIndex);
+        result+=fileName + "\n";
+
+        cursor = nullIndex + 21;
+    }
+
+    process.stdout.write(result)
+}
+
+
+function recursiveCreateTree(dir:string):string{
+     let treeBuffer= Buffer.alloc(0);
+     let entriesArr = [];
+     let fileEntries = fs.readdirSync(dir,{withFileTypes:true})
+     for(const entry of fileEntries){
+        if(entry.name===".git")continue;
+
+        let mode : string;
+        let hash : string;
+
+        const pathName = path.join(dir,entry.name);
+
+        if(entry.isFile()){
+            mode="100644";
+            const fileContent = fs.readFileSync(pathName);
+            hash = generateHash(fileContent, "blob", true);
+
+        }
+        else {
+            mode="40000";
+            hash= recursiveCreateTree(pathName);
+        }
+
+        entriesArr.push({
+            mode:mode,
+            hash : hash,
+            name : entry.name
+        })
+
+    }
+
+    entriesArr.sort((a, b) => a.name.localeCompare(b.name));
+
+
+    for (const entry of entriesArr){
+            const entryHash=Buffer.concat([Buffer.from(`${entry.mode} ${entry.name}\0`),
+                Buffer.from(entry.hash, 'hex')
+            ])
+            treeBuffer=Buffer.concat([treeBuffer, entryHash]);
+    }
+    return generateHash(treeBuffer, "tree", true);
+}
+
+function handleWriteTreeCommand(){
+     const basePath=process.cwd();
+     const treeHash = recursiveCreateTree(basePath);
+     process.stdout.write(treeHash);
 }
